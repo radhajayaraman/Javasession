81670318e0e8804eafc6971f68626851e6fa47fe
91967 <91967@DESKTOP-2F5EMT8>
2023-10-19 12:45:55 +0530

    Committed this code to git

---
+++ JavasessionPro/JavaSessionProjects/src/DoubleLoop/doubleLoopExample.java
@@ -0,0 +1,87 @@
+package DoubleLoop;
+
+public class doubleLoopExample {
+
+	public static void main(String[] args) {
+		/*
+		for(int i=1; i<=5; i=i+1)
+		{
+			for(int j=1; j<=i; j=j+1)
+			{
+				System.out.print("*");
+			}
+			
+			System.out.println();
+		}
+		
+		*/
+		
+		/* pattern 2
+		for(int i=5; i<=5; i=i-1)
+		{
+			for(int j=1; j<=i; j=j+1)
+			{
+				System.out.print("*");
+			}
+			
+			System.out.println();
+			if (i == 0) {
+			    break;
+			}
+		}
+		
+		*/
+		/*
+		for(int i=5; i>=1; i=i-1)
+		{
+			for(int j=1; j<=i; j=j+1)
+			{
+				System.out.print("*");
+			}
+			
+			System.out.println();
+			
+		}
+		*/
+		
+		/*
+		for(int i=1; i<=5; i=i+1)
+		{
+			for(int j=1; j<=i; j=j+1)
+			{
+				System.out.print(0+j);
+			}
+			
+			System.out.println();
+			
+		}
+		*/
+		
+		/*
+		int no = 1;
+		for(int i=1; i<=4; i=i+1)
+		{
+			for(int j=1; j<=i; j=j+1)
+			{
+				System.out.print(no);
+				no=no+1;
+				
+			}
+			
+			System.out.println();
+			
+		}
+		*/
+		for(int i=1; i<=5; i=i+1)
+		{
+			for(int j=1; j<=i; j=j+1)
+			{
+				System.out.print("*");
+							
+			}
+			
+			System.out.println();
+			
+		}
+	}
+}
